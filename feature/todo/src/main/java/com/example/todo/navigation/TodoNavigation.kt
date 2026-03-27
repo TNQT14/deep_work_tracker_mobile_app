a8e3963568adf52db47399865457312b0974efd1
Trương Nguyễn Quang Thái <87488896+TNQT14@users.noreply.github.com>
2026-03-27 10:27:15 +0700

    Add todo navigation graph and update dependencies
    Integrates `todoGraph` into the main application navigation and adds the `androidx.navigation.compose` dependency to the todo feature module. Refactors `TodoListScreen` to use `DropdownMenu` instead of `ExposedDropdownMenu` and cleans up unused imports.

---
+++ feature/todo/src/main/java/com/example/todo/navigation/TodoNavigation.kt
@@ -0,0 +1,33 @@
+package com.example.todo.navigation
+
+import android.net.Uri
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import com.deepworktracker.todo.presentation.TodoListScreen
+import com.example.todo.presentation.tododetail.TodoDetailScreen
+
+object TodoDestinations {
+    const val LIST_ROUTE = "todo"
+    const val DETAIL_ROUTE = "todo/{todoId}"
+
+    fun detailRoute(todoId: String): String = "todo/${Uri.encode(todoId)}"
+}
+
+fun NavGraphBuilder.todoGraph(navController: NavHostController) {
+    composable(TodoDestinations.LIST_ROUTE) {
+        TodoListScreen(
+            onNavigateToTodoDetail = { todoId ->
+                navController.navigate(TodoDestinations.detailRoute(todoId))
+            }
+        )
+    }
+
+    composable(TodoDestinations.DETAIL_ROUTE) { backStackEntry ->
+        val todoId = backStackEntry.arguments?.getString("todoId").orEmpty()
+        TodoDetailScreen(
+            todoId = Uri.decode(todoId),
+            onBack = { navController.popBackStack() }
+        )
+    }
+}
